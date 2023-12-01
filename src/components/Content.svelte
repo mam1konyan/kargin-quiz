f4b7faf6466868bec62c506a9b861d3022f170b1
Paruyr Mamikonyan <paruyr.mamikonyan@cenomi.com>
2023-12-01 15:03:56 +0400

    initial commit

---
+++ src/components/Content.svelte
@@ -0,0 +1,8 @@
+<div class="wrapper"></div>
+
+<style>
+	.wrapper {
+		flex-grow: 1;
+		flex-shrink: 0;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import "$lib";
+	import Header from "../components/Header.svelte";
+	import Footer from "../components/Footer.svelte";
+	import Content from "../components/Content.svelte";
+</script>
+
+<div>
+	<Header />
+	<Content />
+	<Footer />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		max-width: 100vw;
+		min-width: 100vw;
+		min-height: 100vh;
+		margin: 0;
+		padding: 0;
+		background: var(--primary-color);
+		transition: var(--transition-time);
+	}
+</style>
